dfd6b3900c43d5d69767be1971914ad66850e4ab
Anthony <anthony.debassige@rogers.com>
2021-09-09 17:26:05 -0400

    Updated randomized array of encrypted keys

--- php/getToken.php
+++ php/getToken.php
@@ -5,23 +5,24 @@ require_once('generate_token.php');
 require_once('api_get.php');
 require_once('api_post.php');
 $config = include('config.php');
+$randIndex = array_rand($config,1);
 
 $fp = fopen("../keys/secret.pem", "r");
 $priv_key = fread($fp, 8192);
 fclose($fp);
 
 $jwt = generate_jwt ( $priv_key,
-                     $config[0]['ADOBE_ORG_ID'],
-                     $config[0]['ADOBE_TECH_ID'],
+                     $config[$randIndex]['ADOBE_ORG_ID'],
+                     $config[$randIndex]['ADOBE_TECH_ID'],
                      'https://ims-na1.adobelogin.com/c/' .
-                        $config[0]['ADOBE_API_KEY'] 
+                        $config[$randIndex]['ADOBE_API_KEY'] 
                     );
 
-$token = generate_token ( $config[0]['ADOBE_API_KEY'],
-                         $config[0]['ADOBE_API_SECRET'],
+$token = generate_token ( $config[$randIndex]['ADOBE_API_KEY'],
+                         $config[$randIndex]['ADOBE_API_SECRET'],
                          $jwt
                         ); 
 
 $_SESSION["token"] = $token;
-
+$_SESSION['randIndex'] = $randIndex;
 ?>
